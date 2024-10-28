123e4d8319f244fcbad9b3d6e928c7c27a88492c
sarodeMallikarjuna <sarodemallikarjuna5@gmail.com>
2024-10-28 23:09:09 +0530

    my first commit for sping boot project

---
+++ src/main/java/com/example/test/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.test.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class DemoController {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to Demo home page";
+    }
+
+}
